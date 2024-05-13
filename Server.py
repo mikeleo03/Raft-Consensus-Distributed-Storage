0104465bfadc717cc03f884eccbe503b3ec2f54e
mikeleo03 <leonmichael463@gmail.com>
2024-05-13 11:02:00 +0700

    [Feat] Server and logging

---
+++ Server.py
@@ -0,0 +1,26 @@
+from Address       import Address
+from Raft          import RaftNode
+from xmlrpc.server import SimpleXMLRPCServer
+from app           import KVStore
+import sys
+
+
+def start_serving(addr: Address, contact_node_addr: Address):
+    print(f"Starting Raft Server at {addr.ip}:{addr.port}")
+    with SimpleXMLRPCServer((addr.ip, addr.port)) as server:
+        server.register_introspection_functions()
+        server.register_instance(RaftNode(KVStore(), addr, contact_node_addr))
+        server.serve_forever()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: server.py ip port [contact_ip] [contact_port]")
+        exit()
+
+    contact_addr = None
+    if len(sys.argv) == 5:
+        contact_addr = Address(sys.argv[3], int(sys.argv[4]))
+    server_addr = Address(sys.argv[1], int(sys.argv[2]))
+
+    start_serving(server_addr, contact_addr)
---
+++ structs/Log.py
@@ -0,0 +1,6 @@
+from typing import TypedDict
+
+class Log(TypedDict):
+    term: int
+    command: str
+    value: str
