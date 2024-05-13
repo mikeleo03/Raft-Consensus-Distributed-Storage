424736b86311102bb04af3d9af740f0c5a4fae83
mikeleo03 <leonmichael463@gmail.com>
2024-05-13 11:01:46 +0700

    [Feat] Base address class

---
+++ Address.py
@@ -0,0 +1,26 @@
+from typing import TypedDict
+
+class AddressData(TypedDict):
+    ip: str
+    port: int
+
+class Address(dict):
+    def __init__(self, ip: str, port: int):
+        dict.__init__(self, ip=ip, port=port)
+        self.ip = ip
+        self.port = port
+
+    def __str__(self):
+        return f"{self.ip}:{self.port}"
+
+    def __iter__(self):
+        return iter((self.ip, self.port))
+
+    def __eq__(self, other):
+        return self.ip == other.ip and self.port == other.port
+
+    def __ne__(self, other):
+        return self.ip != other.ip or self.port != other.port
+
+    def data(self) -> AddressData:
+        return AddressData(ip=self.ip, port=self.port)
