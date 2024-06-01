c80e755eeb9cdecd2bf7f15488300331e88444f3
dhanikanovlisa <13521132@std.stei.itb.ac.id>
2024-06-02 01:38:33 +0700

    hotfix: append entries

--- Raft.py
+++ Raft.py
@@ -45,20 +45,20 @@ class RaftNode:
 
     def __init__(self, application: KVStore, addr: Address, contact_addr: Address = None):
         socket.setdefaulttimeout(RaftNode.RPC_TIMEOUT)
-        self.address: Address = addr
-        self.type: NodeType = NodeType.FOLLOWER
-        self.log: List[Log] = []
-        self.app: KVStore = application
-        self.election_term: int = 0
-        self.cluster_addr_list: List[Address] = []
-        self.cluster_leader_addr: Address = None
-        self.heartbeat_time: float = time.time()
-        self.timeout_time: float = time.time() + RaftNode.ELECTION_TIMEOUT_MIN + (RaftNode.ELECTION_TIMEOUT_MAX - RaftNode.ELECTION_TIMEOUT_MIN) * random.random()
-        self.is_timeout: bool = False
-        self.current_time: float = time.time()
-        self.votes_received: Set[Address] = set()
-        self.ack_length: Dict[Address, int] = {}
-        self.sent_length: Dict[Address, int] = {}
+        self.address:             Address           = addr
+        self.type:                NodeType          = NodeType.FOLLOWER
+        self.log:                 List[Log]         = []
+        self.app:                 KVStore           = application
+        self.election_term:       int               = 0
+        self.cluster_addr_list:   List[Address]     = []
+        self.cluster_leader_addr: Address           = None
+        self.heartbeat_time:      float             = time.time()
+        self.timeout_time:        float             = time.time() + RaftNode.ELECTION_TIMEOUT_MIN + (RaftNode.ELECTION_TIMEOUT_MAX - RaftNode.ELECTION_TIMEOUT_MIN) * random.random()
+        self.is_timeout:          bool              = False
+        self.current_time:        float             = time.time()
+        self.votes_received:    Set[Address] 
+        self.ack_length:        Dict[Address, int]  = {}
+        self.sent_length:       Dict[Address, int]  = {}
         
         # Get state from stable storage
         self.__fetch_stable_storage()
@@ -135,7 +135,7 @@ class RaftNode:
                 break
         self.__print_log("Starting election...")
 
-    def send_heartbeat_msg(self, addr):
+    def send_heartbeat_msg(self, addr: Address):
         with self.stable_storage as stable_vars:
             prev_last_index = self.sent_length.get(addr, 0)
             next_index = stable_vars["log"][prev_last_index:]
@@ -148,18 +148,18 @@ class RaftNode:
                 "entries": next_index,
                 "leader_commit": stable_vars["commit_length"],
             }
-        
-        print("request", request)
-        response = self.__send_request(request, "heartbeat", addr)
-        if response["status"] != ResponseStatus.SUCCESS.value:
-            return
-        
-        ack = response["ack"]
-        with self.stable_storage as stable_vars:
+            
+            response = self.__send_request(request, "heartbeat", addr)
+            if response["status"] != ResponseStatus.SUCCESS.value:
+                return
+            
+            ack = response["ack"]
             if (response["election_term"] == stable_vars["election_term"] and self.type == NodeType.LEADER) and response["sync"]:
+                print("ack", ack)
                 if ack >= self.ack_length.get(addr, 0):
                     self.ack_length[addr] = ack
                     self.sent_length[addr] = ack
+                    print("Commit")
                     self.__commit_log(stable_vars)
                     
             elif response["election_term"] > stable_vars["election_term"]:
@@ -242,10 +242,12 @@ class RaftNode:
     def heartbeat(self, json_request: str) -> "json":
         self.randomize_timeout()
         request = self.message_parser.deserialize(json_request)
+        print("Sebelum stabel var")
         with self.stable_storage as stable_vars:
             if request["election_term"] == stable_vars["election_term"]:
+                print("Sama termnya")
                 self.type = NodeType.FOLLOWER
-                self.cluster_leader_addr = request["leader_addr"]
+                self.cluster_leader_addr = Address(**request["leader_addr"])
                 self.votes_received = set()
             elif request["election_term"] > stable_vars["election_term"]:
                 stable_vars.update({
@@ -268,7 +270,9 @@ class RaftNode:
             }
             print("ack_length", self.ack_length)
             if all_sync:
+                print("Masuk all sync")
                 self.__append_entries(request["entries"], request["prev_last_index"], request["leader_commit"], stable_vars)
+                print("Kesini")
                 ack = request["prev_last_index"] + len(request["entries"])
                 response["ack"] = ack
                 response["sync"] = True
@@ -298,12 +302,27 @@ class RaftNode:
             self.__print_log(f"Committed up to index {latest_ack}")
 
     def __append_entries(self, entries, prev_last_index, leader_commit, stable_var):
-        if prev_last_index >= len(stable_var["log"]) or stable_var["log"][prev_last_index]["term"] != entries[0]["term"]:
-            stable_var["log"] = stable_var["log"][:prev_last_index]
-            stable_var["log"] += entries
-            self.stable_storage.storeAll(stable_var)
-            self.__commit_log(stable_var)
-        return
+        log = stable_var["log"]
+
+        if len(entries) > 0 and len(log) > prev_last_index:
+            idx = min(len(log), prev_last_index + len(entries)) - 1
+            if log[idx]["term"] != entries[idx - prev_last_index]["term"]:
+                log = log[:prev_last_index]
+        
+        if prev_last_index + len(entries) > len(log):
+            for i in range(len(log) - prev_last_index, len(entries)):
+                log.append(entries[i])
+        
+        stable_var["log"] = log
+
+        commit_length = stable_var["commit_length"]
+        if leader_commit > commit_length:
+            for i in range(commit_length, leader_commit):
+                # TODO: deliver log to application
+                self.app.executing_log(log[i])
+            stable_var["commit_length"] = leader_commit
+
+        self.stable_storage.storeAll(stable_var)
 
     # Client RPCs
     def execute(self, json_request: str) -> str:
